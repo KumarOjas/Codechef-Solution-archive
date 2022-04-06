40667ac7f437d3cf2e4ef0060e7a1803b0233a50
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-04-06 14:12:59 +0530

    Create Andsubar.java

---
+++ Andsubar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  
+ 
+    public  static void main (String [] args){
+    
+   Scanner sc = new Sanner(System.in);
+    
+   
+     int T=sc.nextInt();
+    
+    for (int tc = 0; tc < T; ++tc) {
+     
+       int N = sc.nextInt();
+
+      
+       System.out.println(solve(N));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    int result = 0;
+    for (long i = 1; i <= N; i <<= 1) {
+      result = Math.max(result, (int) Math.min(i, N + 1 - i));
+    }
+
+    return result;
+  }
+}
